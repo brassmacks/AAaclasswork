61c495a880b33bbc3ce30946c305f17539ef70e8
Kiet Nguyen <kiet9657@yahoo.com>
2021-06-03 14:44:21 -0700

    On my_tranpose

--- W4D4/lib/tdd.rb
+++ W4D4/lib/tdd.rb
@@ -4,3 +4,18 @@ def my_uniq(arr)
     arr.each { |ele| res << ele if !res.include?(ele) }
     res
 end
+
+class Array
+
+    def two_sum
+        return [] if self.length <= 1
+        pairs = []
+        (0...self.length).each do |ind1|
+            (ind1+1...self.length).each do |ind2|
+                pairs << [ind1, ind2] if self[ind1] + self[ind2] == 0
+            end
+        end
+        pairs
+    end
+
+end
--- W4D4/spec/tdd_spec.rb
+++ W4D4/spec/tdd_spec.rb
@@ -14,4 +14,35 @@ describe "my_uniq" do
 
 end
 
-#check if method removes duplicates
+describe "two_sum" do
+    it "returns array of pairs of indices that sum to 0" do
+        expect([-1, 0 , 2 , -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+    end
+
+    it "returns empty array if no elements pair" do
+        expect([5, 4 , 3 , 2, 1].two_sum).to eq([])
+    end
+
+end
+
+describe "my_transpose" do 
+    #let(:board) { double(:board, :valid_pos? => true) }
+    let(:rows) { [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+      ] } 
+    let(:cols) { [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8]
+    ]) }
+
+    it "returns the result of switching indices of a 2D array"
+        expect(rows.my_transpose).to eq(cols)
+    end
+
+    it "raise error if not a square 2D array"
+        expect()
+    end
+end
