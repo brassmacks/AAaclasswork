32eb35def9175bab958e154c4ce98669ae02c61f
lbrilli <lbrilli11@gmail.com>
2021-06-04 15:34:15 -0600

    'largest continous sub_sum part 1, begin part 2'

--- W4D5/exe_time_diff.rb
+++ W4D5/exe_time_diff.rb
@@ -1,4 +1,4 @@
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# list = [ 0, 3, 5, 4, 5, 1, 1, 2 ]
 
 def my_min1(list)
     min = list[0]
@@ -26,4 +26,40 @@ def my_min_2(list)
     min
 end
 
-p my_min1(list)
+# p my_min1(list)
+
+list = [5, 3, -7]
+
+# [2, 3, -6, 7, -6, 7]
+# [-5, -1, -3]
+
+# def largest_continuous_sub_sum(list)
+#     arr = []
+
+#     (0...list.length-2).each do |idx1|
+#         (idx1+1...list.length-1).each do |idx2|
+#            arr << [list[idx1] + list[idx2],list[idx2+1]]
+#         end
+#     end
+#     p arr
+#     arr.map {|sub| sub.sum}.max
+# end
+
+# p largest_continuous_sub_sum(list)
+
+def largest_continuous_sub_sum(list)
+    count = 1
+    cur_max = list[0] + list[1] + list[2]
+    cur_sum = 0
+    limit = list.length-1
+
+    while count < limit
+        cur_sum = list[count-1] + list[count] + list[count+1]
+        cur_max = cur_sum if cur_max < cur_sum
+        count += 1
+    end
+
+    cur_max
+end
+
+p largest_continuous_sub_sum(list)
