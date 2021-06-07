c8287dea87568ba87dcea4fb8458701adc30614b
Joshua Elliott <jadepavedpath@gmail.com>
2021-06-07 13:39:01 -0700

    w5d1 init

--- W4D5/exe_time_diff.rb
+++ W4D5/exe_time_diff.rb
@@ -1,5 +1,5 @@
 # list = [ 0, 3, 5, 4, 5, 1, 1, 2 ]
-
+require "byebug"
 def my_min1(list)
     min = list[0]
     list.each_with_index do |ele1,idx1|
@@ -73,24 +73,26 @@ def largest_continuous_sub_sum(list)
         if list[counter].abs > curr_sum && list[counter] < list[counter].abs
             valleys << counter
             curr_sum = list[counter+1] if counter < list.length-2
-        elsif curr_sum + list[counter] > curr_sum
+        elsif curr_sum + list[counter] > curr_sum && valleys[-1] != counter - 1
             curr_sum += list[counter] 
+        else
+            curr_sum = list[counter]
         end
+
         max_sum = curr_sum if max_sum < curr_sum
         counter += 1
     end
-
     valleys.map do |valley|
-        val_total = list.take(valley) + [list[valley]] + list.drop(valley+1)
-        val_total = val_total.sum
-        if val_total > list.take(valley) && val_total > list.drop(valley+1)
+        debugger
+        val_total = list.take(valley).sum + list[valley] + list.drop(valley+1).sum
+        if val_total > list.take(valley).sum && val_total > list.drop(valley+1).sum
             if max_sum < val_total
                 max_sum = val_total
             end
         end
     end
 
-    max_sum
+    max_sum > list.sum ? max_sum : list.sum 
 end
 
 p largest_continuous_sub_sum(list)
