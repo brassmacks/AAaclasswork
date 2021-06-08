ba7455783dac714cb2e20f4c5ad0928fa0a6f9b5
Samuel Song <samsongs@umich.edu>
2021-06-08 16:22:07 -0600

    Completed part 2 of sql zoo project

--- w5D2/skeleton/lib/01_select_basics.rb
+++ w5D2/skeleton/lib/01_select_basics.rb
@@ -40,7 +40,7 @@ def per_capita_gdp
 
   FROM countries
 
-  WHERE area > 5000000 
+  WHERE area > 5000000
   SQL
 end
 
@@ -63,15 +63,18 @@ def scandinavia
   SELECT name, population
   FROM countries
   WHERE name = 'Denmark'
-  OR name = 'Finland' 
-  OR name = 'Norway' 
-  OR name = 'Sweden' 
+  OR name = 'Finland'
+  OR name = 'Norway'
+  OR name = 'Sweden'
   SQL
 end
 
 def starts_with_g
   # Show each country that begins with the letter G
   execute(<<-SQL)
+  SELECT name
+  FROM countries
+  WHERE name LIKE('G%')
   SQL
 end
 
@@ -80,5 +83,8 @@ def just_the_right_size
   # with an area between 200,000 and 250,000.
   # BETWEEN allows range checking - note that it is inclusive.
   execute(<<-SQL)
+    SELECT name, area/1000
+    FROM countries
+    WHERE area BETWEEN 200000 AND 250000
   SQL
 end
