f8d5cc453ee5b8457a583d4d7066da2667379ca9
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-06-17 11:23:38 -0700

    Artwork Migration

--- W6D3/second_routes/first_routes/db/schema.rb
+++ W6D3/second_routes/first_routes/db/schema.rb
@@ -10,14 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_171409) do
+ActiveRecord::Schema.define(version: 2021_06_17_174828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "artworks", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "image_url", null: false
+    t.integer "artist_id", null: false
+    t.index ["artist_id", "title"], name: "index_artworks_on_artist_id_and_title", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
+    t.string "username", null: false
   end
 
 end
