449b1c7e44c2a65c37395a9d05b6ae3294515ecb
b-chai <b.chai82@gmail.com>
2021-06-18 20:00:17 -0400

    did things

--- W6D5/views_99Cats/app/controllers/cats_controller.rb
+++ W6D5/views_99Cats/app/controllers/cats_controller.rb
@@ -1,12 +1,32 @@
 class CatsController < ApplicationController
 
     def index
-        @cats = Cats.all
+        @cats = Cat.all
         render :index
     end
     
     def show
-        @cat = Cats[param[:id]]
+        @cat = Cat.find(params[:id])
         render :show
+    end
+
+    def new
+        @cat = Cat.new
+        render :new
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render :new
+        end
+    end
+
+    private
+    def cat_params
+        params.require(:cat).permit(:name,:birth_date,:color,:sex,:description)
     end
 end
--- W6D5/views_99Cats/app/models/cat.rb
+++ W6D5/views_99Cats/app/models/cat.rb
@@ -1,3 +1,6 @@
+require 'action_view'
 class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
+    COLORS = ["red", "brown", "yellow", "orange", "spotty", "persian", "blonde"]
 
 end
