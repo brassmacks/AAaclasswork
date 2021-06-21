1ea222165a8a8a4402959b9fa25014ccd56459f8
Joshua Elliott <jadepavedpath@gmail.com>
2021-06-21 14:10:40 -0700

    "Users model and validations"

--- W7D1/skeleton/db/seeds.rb
+++ W7D1/skeleton/db/seeds.rb
@@ -2,6 +2,10 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+colors = ["red", "brown", "yellow", "orange", "spotty", "persian", "blonde"]
+sexs = ["M", "F"]
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# 20.times do Cat.create!(name: Faker::Name.name, birth_date: Faker::Date.backward(days: 4000), color: colors.sample, sex: sexs.sample)
+
+end
