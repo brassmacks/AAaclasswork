89ea66e52d8f1295000e7738791d4bb871a9610e
Shirley Tang <shirleytang1996@gmail.com>
2021-07-06 18:32:19 -0400

    finish utils and move

--- JS/9.1/Asteroids/dist/main.js
+++ JS/9.1/Asteroids/dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("const MovingObject = __webpack_require__(/*! ./moving_object.js */ \"./src/moving_object.js\");\nwindow.MovingObject = MovingObject;\n\n\n\nconst screen = document.getElementById(\"game-canvas\");\nconst ctx = screen.getContext(\"2d\");\n\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("const MovingObject = __webpack_require__(/*! ./moving_object.js */ \"./src/moving_object.js\");\nwindow.MovingObject = MovingObject;\nconst mo = new MovingObject({\n    pos: [30, 30],\n    vel: [10, 10],\n    radius: 5,\n    color: \"#00FF00\"\n  });\n\ndocument.addEventListener(\"DOMContentLoaded\", function(){\n    const screen = document.getElementById(\"game-canvas\");\n    const ctx = screen.getContext(\"2d\");\n      mo.draw(ctx);\n      mo.move();\n      \n});\n\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ }),
 
@@ -25,7 +25,7 @@ eval("const MovingObject = __webpack_require__(/*! ./moving_object.js */ \"./src
   \******************************/
 /***/ ((module) => {
 
-eval("// base class for anything that moves\n\n\nfunction MovingObject(options) {\n\n    this.pos = options[pos]; // 2d array x,y\n    this.vel = options[vel]; \n    this.radius = options[radius];\n    this.color = options[color];\n\n}\n// MovingObject.prototype.move\n\n\n// MovingObject.prototype.draw(ctx)\nMovingObject.prototype.draw = function(ctx){\n    ctx.fillStyle = this.color;\n    ctx.beginPath();\n    ctx.arc(this.pos[0],this.pos[1], this.radius, 0, 2*Math.PI, false);\n    ctx.fill();\n    \n}\n\n// MovingObject.prototype.isCollidedWith(otherMovingObject)\n\nmodule.exports = MovingObject;\n\n//# sourceURL=webpack:///./src/moving_object.js?");
+eval("// base class for anything that moves\n\n\nfunction MovingObject(options) {\n    //  debugger\n    this.pos = options.pos; // 2d array x,y\n    this.vel = options.vel; \n    this.radius = options.radius;\n    this.color = options.color;\n\n}\n// MovingObject.prototype.move\nMovingObject.prototype.move = function(){\n    this.pos = [this.pos[0]+this.vel[0], this.pos[1]+this.vel[1]];\n    \n}\n\n\n// MovingObject.prototype.draw(ctx)\nMovingObject.prototype.draw = function(ctx){\n   \n    ctx.beginPath();\n    ctx.fillStyle = this.color; \n    ctx.arc(this.pos[0],this.pos[1], this.radius, 0, 2*Math.PI, false);\n    ctx.fill();\n\n}\n\n// MovingObject.prototype.isCollidedWith(otherMovingObject)\nMovingObject.prototype.collideWith = function(otherObject) {\n\n};\n\nmodule.exports = MovingObject;\n\n//# sourceURL=webpack:///./src/moving_object.js?");
 
 /***/ })
 
--- JS/9.1/Asteroids/src/moving_object.js
+++ JS/9.1/Asteroids/src/moving_object.js
@@ -2,21 +2,25 @@
 
 
 function MovingObject(options) {
-
-    this.pos = options[pos]; // 2d array x,y
-    this.vel = options[vel]; 
-    this.radius = options[radius];
-    this.color = options[color];
+    //  debugger
+    this.pos = options.pos; // 2d array x,y
+    this.vel = options.vel; 
+    this.radius = options.radius;
+    this.color = options.color;
 
 }
 // MovingObject.prototype.move
-MovingObject.prototype.move = function(){}
+MovingObject.prototype.move = function(){
+    this.pos = [this.pos[0]+this.vel[0], this.pos[1]+this.vel[1]];
+    
+}
 
 
 // MovingObject.prototype.draw(ctx)
 MovingObject.prototype.draw = function(ctx){
-    ctx.fillStyle = this.color;
+   
     ctx.beginPath();
+    ctx.fillStyle = this.color; 
     ctx.arc(this.pos[0],this.pos[1], this.radius, 0, 2*Math.PI, false);
     ctx.fill();
 
