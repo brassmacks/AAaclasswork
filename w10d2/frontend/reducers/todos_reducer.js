56b5def7e3836f7d1e50efe89324eaa5bba5b12f
Jay Lam <lamjay415@gmail.com>
2021-07-20 17:20:00 -0700

    completed steps reducer

--- w10d2/frontend/reducers/todos_reducer.js
+++ w10d2/frontend/reducers/todos_reducer.js
@@ -1,36 +1,22 @@
 import { RECEIVE_TODOS, RECEIVE_TODO, REMOVE_TODO } from "../actions/todo_actions";
 
-const initialState = {
-    // 1: {
-        // id: 1,
-        // title: "wash car",
-        // body: "with soap",
-        // done: false
-    // },
-    // 2: {
-        // id: 2,
-        // title: "wash dog",
-        // body: "with shampoo",
-        // done: true
-    // }
-};
 
 
-const todos_reducer = (state = initialState, action) => {
+const todos_reducer = (state = {}, action) => {
     Object.freeze(state);
     let nextState = Object.assign({},state);
-    
+    // debugger;
     switch(action.type){
         case REMOVE_TODO:
-            nextState.remove(todo.id)
+            delete nextState[action.todo.id];
             return nextState;
         case RECEIVE_TODOS:
             action.todos.forEach((todo)=> {
                 nextState[todo.id] = todo;            
-            })
+            });
             return nextState;
         case RECEIVE_TODO:
-            nextState[action.id] = action.todos;
+            nextState[action.todo.id] = action.todo;
             return nextState;
         default: 
             return state;
