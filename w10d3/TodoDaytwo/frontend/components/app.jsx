dd6943ef824ec54e4f01f01481fbd3d12ead5666
brassmacks <jadepavedpath@gmail.com>
2021-07-21 16:36:56 -0700

    'redux stash redoo'
    merging branched
    
    merge 2nd attempt
    
    module replacement

--- w10d3/TodoDaytwo/frontend/components/app.jsx
+++ w10d3/TodoDaytwo/frontend/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { TodoListContainer } from './todos/todo_list_container'
 
 class App extends React.Component {
   constructor(props){
@@ -8,7 +9,10 @@ class App extends React.Component {
   
 render() {
   return (
-    <h1>Todos</h1>
+    <div>Todos
+      <TodoListContainer />
+    </div>
+  
   )
 }
 }
